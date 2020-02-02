8eb74e738ff679e957735c19b121196d3483bd2e
AtariAmiga <vincent.agami@gmail.com>
2020-02-02 15:32:37 +0100

    Pose des questions au hasard et dit bravo

--- interrogation_hasard.py
+++ interrogation_hasard.py
@@ -3,11 +3,12 @@ from random import randrange
 liste_questions = []
 
 # Je construis la liste des questions
-for i in range(1, 11):
-    for j in range(1, 11):
+for i in range(1, 4):
+    for j in range(1, 4):
         question = (i, j)
         liste_questions.append(question)
 
+mauvaises_reponses = []
 while len(liste_questions) > 0:
     n = randrange(len(liste_questions))
     question_au_hasard = liste_questions.pop(n)
@@ -18,4 +19,8 @@ while len(liste_questions) > 0:
         print(i, 'x', j, '= ?')
         r = int(input())
         if r == i * j:
+            print( "Bravo !")
             break
+        else:
+            print( "Tu t'es trompé, recommence" )
+            mauvaises_reponses.append(question_au_hasard)
