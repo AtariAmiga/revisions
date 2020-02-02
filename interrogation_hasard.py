4f05f84b91a6af97f09b268e02e83d5a0bb8f037
AtariAmiga <vincent.agami@gmail.com>
2020-02-02 15:42:41 +0100

    Fait réviser les erreurs

--- interrogation_hasard.py
+++ interrogation_hasard.py
@@ -3,24 +3,38 @@ from random import randrange
 liste_questions = []
 
 # Je construis la liste des questions
-for i in range(1, 4):
-    for j in range(1, 4):
+for i in range(1, 11):
+    for j in range(1, 11):
         question = (i, j)
         liste_questions.append(question)
 
-mauvaises_reponses = []
-while len(liste_questions) > 0:
-    n = randrange(len(liste_questions))
-    question_au_hasard = liste_questions.pop(n)
-    i = question_au_hasard[0]
-    j = question_au_hasard[1]
+while True:
+    mauvaises_reponses = []
+    while len(liste_questions) > 0:
+        n = randrange(len(liste_questions))
+        question_au_hasard = liste_questions.pop(n)
+        i = question_au_hasard[0]
+        j = question_au_hasard[1]
 
-    while True:
-        print(i, 'x', j, '= ?')
-        r = int(input())
-        if r == i * j:
-            print( "Bravo !")
-            break
-        else:
-            print( "Tu t'es trompé, recommence" )
-            mauvaises_reponses.append(question_au_hasard)
+        while True:
+            print(i, 'x', j, '= ?')
+            while True:
+                try:
+                    r = int(input())
+                    break
+                except ValueError:
+                    pass
+
+            if r == i * j:
+                print( "Bravo !")
+                break
+            else:
+                print( "Tu t'es trompé, recommence" )
+                mauvaises_reponses.append(question_au_hasard)
+
+    liste_questions = mauvaises_reponses
+    if len(mauvaises_reponses) == 0:
+        print( "Bravo, tu as tout bien révisé !")
+        break
+    else:
+        print( "Bravo, on va réviser les mauvaises réponses")
